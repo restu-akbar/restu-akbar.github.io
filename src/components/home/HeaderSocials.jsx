ec970ed1c322cb10dd61f1fe579b8a3032ccabf9
restu <restu.akbar.03.04@gmail.com>
2025-07-19 12:10:15 +0700

    add: gitlab acc

--- src/components/home/HeaderSocials.jsx
+++ src/components/home/HeaderSocials.jsx
@@ -1,18 +1,37 @@
-import React from 'react';
-import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
+import React from "react";
+import { FaGithub, FaLinkedinIn, FaGitlab } from "react-icons/fa";
 
 const HeaderSocials = () => {
-    return (
-        <div className='home__socials'>
-            <a href='https://www.github.com/akbarst3' className='home__social-link' target='_blank' rel='noreferrer'>
-                <FaGithub />
-            </a>
+  return (
+    <div className="home__socials">
+      <a
+        href="https://www.github.com/akbarst3"
+        className="home__social-link"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <FaGithub />
+      </a>
 
-            <a href='https://www.linkedin.com/in/restuakbar' className='home__social-link' target='_blank' rel='noreferrer'>
-                <FaLinkedinIn />
-            </a>
-        </div>
-    );
+      <a
+        href="https://www.linkedin.com/in/restuakbar"
+        className="home__social-link"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <FaLinkedinIn />
+      </a>
+      <a
+        href="https://www.gitlab.com/akbarst3"
+        className="home__social-link"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <FaGitlab />
+      </a>
+    </div>
+  );
 };
 
 export default HeaderSocials;
+
